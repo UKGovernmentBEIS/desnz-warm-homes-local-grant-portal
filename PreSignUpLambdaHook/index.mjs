7490e4fd12086f34e002de8497391943412066fb
Tim Leach <timleach635@gmail.com>
2023-04-25 15:06:08 +0100

    feat: Add Lambda deploy action

---
+++ PreSignUpLambdaHook/index.mjs
@@ -0,0 +1,20 @@
+﻿export const handler = async (event, context, callback) => {
+    const allowedEmailAddresses = [
+        "test1@example.com"
+    ];
+
+    // case-insensitive string comparison, see
+    // https://stackoverflow.com/questions/2140627/how-to-do-case-insensitive-string-comparison
+    if (!allowedEmailAddresses.some(
+        ea => ea.localeCompare(
+            event.request.userAttributes.email,
+            "en-GB",
+            { sensitivity: "accent" }
+        ) === 0)
+    ) {
+        var error = new Error("Email address not allowed");
+        callback(error, event);
+    }
+
+    callback(null, event);
+};
