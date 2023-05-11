0340033a862181e1fe1d969e98a69eb35e9550be
Tim Leach <timleach635@gmail.com>
2023-05-11 14:08:26 +0100

    feat: Add tests for view model

---
+++ HerPortal.UnitTests/Website/Models/HomepageViewModelTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using HerPortal.BusinessLogic.Models;
+using HerPortal.Models;
+using NUnit.Framework;
+
+namespace Tests.Website.Models;
+
+[TestFixture]
+public class HomepageViewModelTests
+{
+    [Test]
+    public void ShouldShowBannerIfUserHasNotLoggedIn()
+    {
+        // Arrange
+        var user = new User
+        {
+            HasLoggedIn = false,
+        };
+        
+        // Act
+        var viewModel = new HomepageViewModel(user);
+        
+        // Assert
+        viewModel.ShouldShowBanner.Should().BeTrue();
+    }
+    
+    [Test]
+    public void ShouldNotShowBannerIfUserHasLoggedIn()
+    {
+        // Arrange
+        var user = new User
+        {
+            HasLoggedIn = true,
+        };
+        
+        // Act
+        var viewModel = new HomepageViewModel(user);
+        
+        // Assert
+        viewModel.ShouldShowBanner.Should().BeFalse();
+    }
+}
