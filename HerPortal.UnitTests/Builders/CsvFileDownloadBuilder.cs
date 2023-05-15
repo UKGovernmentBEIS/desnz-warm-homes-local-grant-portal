a0435b9fe2fd19e88835adc8f8f74c9c4b58c2f5
Tim Leach <timleach635@gmail.com>
2023-05-15 13:45:30 +0100

    test: Add tests for the CSV file downloading classes

---
+++ HerPortal.UnitTests/Builders/CsvFileDownloadBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using HerPortal.BusinessLogic.Models;
+
+namespace Tests.Builders;
+
+public class CsvFileDownloadBuilder
+{
+    private CsvFileDownload csvFileDownload;
+
+    public CsvFileDownloadBuilder(int month)
+    {
+        csvFileDownload = new CsvFileDownload
+        {
+            User = new User(),
+            DateTime = new DateTime(2023, month, 1),
+        };
+    }
+
+    public CsvFileDownload Build()
+    {
+        return csvFileDownload;
+    }
+
+    public CsvFileDownloadBuilder WithUserWithId(int userId)
+    {
+        csvFileDownload.User.Id = userId;
+        return this;
+    }
+}
---
+++ HerPortal.UnitTests/BusinessLogic/Models/CsvFileDownloadDataTests.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using HerPortal.BusinessLogic.Models;
+using NUnit.Framework;
+using Tests.Builders;
+
+namespace Tests.BusinessLogic.Models;
+
+[TestFixture]
+public class CsvFileDownloadDataTests
+{
+    [Test]
+    public void CsvFileDownloadData_WhenThereAreNoDownloads_GivesNullLastDownload()
+    {
+        // Arrange
+        var underTest = new CsvFileDownloadData
+        {
+            Downloads = new List<CsvFileDownload>(),
+        };
+        
+        // Act
+        var result = underTest.LastDownload;
+        
+        // Assert
+        result.Should().BeNull();
+    }
+    
+    [Test]
+    public void CsvFileDownloadData_WhenThereAreNoDownloads_GivesNullLastDownloaded()
+    {
+        // Arrange
+        var underTest = new CsvFileDownloadData
+        {
+            Downloads = new List<CsvFileDownload>(),
+        };
+        
+        // Act
+        var result = underTest.LastDownloaded;
+        
+        // Assert
+        result.Should().BeNull();
+    }
+    
+    [Test]
+    public void CsvFileDownloadData_WhenThereAreNoDownloads_GivesNullLastDownloadedBy()
+    {
+        // Arrange
+        var underTest = new CsvFileDownloadData
+        {
+            Downloads = new List<CsvFileDownload>(),
+        };
+        
+        // Act
+        var result = underTest.LastDownloadedBy;
+        
+        // Assert
+        result.Should().BeNull();
+    }
+    
+    [Test]
+    public void CsvFileDownloadData_WhenThereAreDownloads_GivesLatestAsLastDownload()
+    {
+        // Arrange
+        var underTest = new CsvFileDownloadData
+        {
+            Downloads = new List<CsvFileDownload>
+            {
+                new CsvFileDownloadBuilder(1).Build(),
+                new CsvFileDownloadBuilder(2).Build(),
+            },
+        };
+        
+        // Act
+        var result = underTest.LastDownload;
+        
+        // Assert
+        result.Should().NotBeNull();
+        result.DateTime.Month.Should().Be(2);
+    }
+    
+    [Test]
+    public void CsvFileDownloadData_WhenThereAreDownloads_GivesLatestAsLastDownloaded()
+    {
+        // Arrange
+        var underTest = new CsvFileDownloadData
+        {
+            Downloads = new List<CsvFileDownload>
+            {
+                new CsvFileDownloadBuilder(1).Build(),
+                new CsvFileDownloadBuilder(2).Build(),
+            },
+        };
+        
+        // Act
+        var result = underTest.LastDownloaded;
+        
+        // Assert
+        result.Should().NotBeNull();
+        result!.Value.Month.Should().Be(2);
+    }
+    
+    [Test]
+    public void CsvFileDownloadData_WhenThereAreDownloads_GivesLatestAsLastDownloadedBy()
+    {
+        // Arrange
+        var underTest = new CsvFileDownloadData
+        {
+            Downloads = new List<CsvFileDownload>
+            {
+                new CsvFileDownloadBuilder(1).WithUserWithId(1).Build(),
+                new CsvFileDownloadBuilder(2).WithUserWithId(2).Build(),
+            },
+        };
+        
+        // Act
+        var result = underTest.LastDownloadedBy;
+        
+        // Assert
+        result.Should().NotBeNull();
+        result.Id.Should().Be(2);
+    }
+}
