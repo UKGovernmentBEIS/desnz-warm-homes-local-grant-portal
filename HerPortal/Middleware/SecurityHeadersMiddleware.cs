efe7595e804d515d3c8e934c88e44b788085b433
Joe Edwards <Joe.Edwards@softwire.com>
2023-06-16 16:57:53 +0100

    feat: Copy strict CSP from website

--- HerPortal/Middleware/SecurityHeadersMiddleware.cs
+++ HerPortal/Middleware/SecurityHeadersMiddleware.cs
@@ -1,10 +1,14 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Http;
 
 namespace HerPortal.Middleware;
 
 public class SecurityHeadersMiddleware
 {
+    public const string NonceContextKey = "ContentSecurityPolicyScriptNonce";
+
     private readonly RequestDelegate next;
 
     public SecurityHeadersMiddleware(RequestDelegate next)
@@ -18,14 +22,19 @@ public class SecurityHeadersMiddleware
         {
             context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
         }
-        
+
+        var nonce = Guid.NewGuid().ToString("N");;
+        context.Items[NonceContextKey] = nonce;
+
         if (!context.Response.Headers.ContainsKey("Content-Security-Policy"))
         {
+            // Based on https://csp.withgoogle.com/docs/strict-csp.html
             context.Response.Headers.Add("Content-Security-Policy",
-                "default-src 'self'; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src  * 'unsafe-inline'"); 
-
+                "object-src 'none'; " +
+                $"script-src 'nonce-{nonce}' 'unsafe-inline' 'strict-dynamic' https:; " +
+                "base-uri 'none';");
         }
-        
+
         if (!context.Response.Headers.ContainsKey("Referrer-Policy"))
         {
             context.Response.Headers.Add("Referrer-Policy", "no-referrer");
@@ -34,4 +43,12 @@ public class SecurityHeadersMiddleware
 
         return next(context);
     } 
+}
+
+public static class SecurityContextExtensions
+{
+    public static HtmlString GetScriptNonce(this HttpContext context)
+    {
+        return new HtmlString((string) context.Items[SecurityHeadersMiddleware.NonceContextKey] ?? "");
+    }
 }
