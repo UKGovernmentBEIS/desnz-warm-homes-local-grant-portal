8fdb837e1cada79ef177932c23fdc0457144057e
Dan Corder <dan.corder@softwire.com>
2023-06-29 16:04:15 +0100

    feat: add SQL for modifying user permissions

---
+++ SQL/modify_user.sql
@@ -0,0 +1,12 @@
+﻿-- Remove LA permission for user
+DELETE FROM "LocalAuthorityUser"
+WHERE "UsersId" IN (SELECT "Id" FROM "Users" WHERE "EmailAddress" = '<user email address>')
+  AND "LocalAuthoritiesId" IN (SELECT "Id" FROM "LocalAuthorities" WHERE "CustodianCode" = '<local authority custodian code>');
+
+-- Add LA permission for user
+INSERT INTO "LocalAuthorityUser" ("LocalAuthoritiesId", "UsersId")
+SELECT "LocalAuthorities"."Id", "Users"."Id"
+FROM "LocalAuthorities" CROSS JOIN "Users"
+WHERE
+    ("Users"."EmailAddress" = '<user email address>' AND "LocalAuthorities"."CustodianCode" = '<local authority custodian code>')
+ON CONFLICT DO NOTHING;
