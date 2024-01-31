49b44d6d8b64bfa018eb7a20a85450fbc7516ba0
Samuel Young <samuel.young@softwire.com>
2024-01-31 17:27:58 +0000

    PC-597: commonise file urls

--- HerPortal/Controllers/CsvFileController.cs
+++ HerPortal/Controllers/CsvFileController.cs
@@ -29,45 +29,29 @@ public class CsvFileController : Controller
     [HttpGet("/la/{custodianCode}/{year:int}/{month:int}")]
     public async Task<IActionResult> GetLaCsvFile(string custodianCode, int year, int month)
     {
-        Stream file;
-        try
-        {
-            file = await csvFileService.GetLocalAuthorityFileForDownloadAsync(custodianCode, year, month, HttpContext.User.GetEmailAddress());
-        }
-        catch (SecurityException ex)
-        {
-            // If this is happening, someone is trying to get around the access controls or there's a bug
-            logger.LogWarning(ex.Message);
-            return Unauthorized("The logged-in user is not permitted to access this resource.");
-        }
-        catch (ArgumentOutOfRangeException)
-        {
-            return NotFound();
-        }
-        catch (Exception ex)
-        {
-            logger.LogError
-            (
-                ex,
-                "Error encountered when attempting to get CSV file with custodian code {CustodianCode}, year {Year}, month {Month}",
-                custodianCode,
-                year,
-                month
-            );
-            return Problem($"An error occured while trying to access the CSV file with custodian code {custodianCode}, year {year}, month {month}.");
-        }
-
-        return File(file, "text/csv", $"{custodianCode}_{year}-{month:D2}.csv");
+        return await HandleAccessingFile(
+            async () => await csvFileService.GetLocalAuthorityFileForDownloadAsync(custodianCode, year, month, HttpContext.User.GetEmailAddress()),
+            $"{custodianCode}_{year}-{month:D2}.csv",
+            $"An error occured while trying to access the CSV file with custodian code {custodianCode}, year {year}, month {month}."
+        );
     }
     
     [HttpGet("/consortium/{consortiumCode}/{year:int}/{month:int}")]
     public async Task<IActionResult> GetConsortiumCsvFile(string consortiumCode, int year, int month)
     {
+        return await HandleAccessingFile(
+            async () => await csvFileService.GetConsortiumFileForDownloadAsync(consortiumCode, year, month, HttpContext.User.GetEmailAddress()),
+            $"{consortiumCode}_{year}-{month:D2}.csv",
+            $"An error occured while trying to access the CSV file with consortium code {consortiumCode}, year {year}, month {month}."
+        );
+    }
+
+    private async Task<IActionResult> HandleAccessingFile(Func<Task<Stream>> fileAccessor, string fileName, string errorMessage)
+    {
         Stream file;
         try
         {
-            // TODO: share code with above?
-            file = await csvFileService.GetConsortiumFileForDownloadAsync(consortiumCode, year, month, HttpContext.User.GetEmailAddress());
+            file = await fileAccessor();
         }
         catch (SecurityException ex)
         {
@@ -84,14 +68,12 @@ public class CsvFileController : Controller
             logger.LogError
             (
                 ex,
-                "Error encountered when attempting to get CSV file with consortium code {ConsortiumCode}, year {Year}, month {Month}",
-                consortiumCode,
-                year,
-                month
+                "{errorMessage}",
+                errorMessage
             );
-            return Problem($"An error occured while trying to access the CSV file with custodian code {consortiumCode}, year {year}, month {month}.");
+            return Problem(errorMessage);
         }
 
-        return File(file, "text/csv", $"{consortiumCode}_{year}-{month:D2}.csv");
+        return File(file, "text/csv", fileName);
     }
 }
