6e85123dada85ad4f56f29817fd934778623acbf
Samuel Young <samuel.young@softwire.com>
2024-06-13 15:57:46 +0100

    PC-1047: add consortium codes script

---
+++ SQL/populate_consortium_codes.sql
@@ -0,0 +1,27 @@
+﻿-- Create LA records
+INSERT INTO "Consortia" ("ConsortiumCode") VALUES
+    ('C_0002'), -- Blackpool
+    ('C_0003'), -- Bristol
+    ('C_0004'), -- Broadland
+    ('C_0006'), -- Cambridge
+    ('C_0007'), -- Cambridgeshire & Peterborough Combined Authority
+    ('C_0008'), -- Cheshire East
+    ('C_0010'), -- Cornwall
+    ('C_0012'), -- Darlington Borough Council
+    ('C_0013'), -- Dartford
+    ('C_0014'), -- Devon
+    ('C_0015'), -- Dorset
+    ('C_0016'), -- Eden District Council
+    ('C_0017'), -- Greater London Authority
+    ('C_0021'), -- Lewes
+    ('C_0022'), -- Liverpool City Region
+    ('C_0024'), -- Midlands Net Zero Hub
+    ('C_0027'), -- North Yorkshire County Council
+    ('C_0029'), -- Oxfordshire County Council
+    ('C_0031'), -- Portsmouth
+    ('C_0033'), -- Sedgemoor
+    ('C_0037'), -- Stroud
+    ('C_0038'), -- Suffolk County Council
+    ('C_0039'), -- Surrey County Council
+    ('C_0044')  -- West Devon
+ON CONFLICT DO NOTHING;
