683b0fe98046e92a19b8ed991afedb0f8c085e2d
Samuel Young <samuel.young@softwire.com>
2025-06-13 15:39:12 +0100

    PC-1842: Disallow onboarding to LAs that are already in owned Consortia

--- WhlgPortalWebsite/Models/AssignCodesToDeliveryPartnerViewModel.cs
+++ WhlgPortalWebsite/Models/AssignCodesToDeliveryPartnerViewModel.cs
@@ -13,11 +13,25 @@ public class AssignCodesToDeliveryPartnerViewModel
 
     public IEnumerable<AssignAuthorityViewModel> GetLocalAuthoritiesToAssign()
     {
-        return LocalAuthorities.Select(localAuthority => new AssignAuthorityViewModel
+        return LocalAuthorities.Select(localAuthority =>
         {
-            Name = LocalAuthorityData.LocalAuthorityNamesByCustodianCode[localAuthority.CustodianCode],
-            Code = localAuthority.CustodianCode,
-            AlreadyAssigned = User.LocalAuthorities.Any(userLocalAuthority => userLocalAuthority.CustodianCode == localAuthority.CustodianCode)
+            var alreadyAssignedToLocalAuthority = User
+                .LocalAuthorities
+                .Any(userLocalAuthority => userLocalAuthority.CustodianCode == localAuthority.CustodianCode);
+
+            // if the user manages a consortium, they shouldn't be able to also manage an LA in that consortium
+            var alreadyAssignedToConsortium = User
+                .Consortia
+                .Any(userConsortium => ConsortiumData
+                    .ConsortiumCustodianCodesIdsByConsortiumCode[userConsortium.ConsortiumCode]
+                    .Any(consortiumCustodianCode => consortiumCustodianCode == localAuthority.CustodianCode));
+
+            return new AssignAuthorityViewModel
+            {
+                Name = LocalAuthorityData.LocalAuthorityNamesByCustodianCode[localAuthority.CustodianCode],
+                Code = localAuthority.CustodianCode,
+                AlreadyAssigned = alreadyAssignedToLocalAuthority || alreadyAssignedToConsortium
+            };
         });
     }
 
@@ -27,10 +41,11 @@ public class AssignCodesToDeliveryPartnerViewModel
         {
             Name = ConsortiumData.ConsortiumNamesByConsortiumCode[consortium.ConsortiumCode],
             Code = consortium.ConsortiumCode,
-            AlreadyAssigned = User.Consortia.Any(userConsortium => userConsortium.ConsortiumCode == consortium.ConsortiumCode)
+            AlreadyAssigned =
+                User.Consortia.Any(userConsortium => userConsortium.ConsortiumCode == consortium.ConsortiumCode)
         });
     }
-    
+
     public class AssignAuthorityViewModel
     {
         public string Name { get; set; }
