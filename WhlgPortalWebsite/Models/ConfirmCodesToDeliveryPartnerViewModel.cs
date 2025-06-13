aeeac53399d88faea728707194e5dbd4c5e1bc10
Samuel Young <samuel.young@softwire.com>
2025-06-13 15:29:48 +0100

    PC-1842: Strengthen wording on the confirmation page

--- WhlgPortalWebsite/Models/ConfirmCodesToDeliveryPartnerViewModel.cs
+++ WhlgPortalWebsite/Models/ConfirmCodesToDeliveryPartnerViewModel.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using GovUkDesignSystem.Attributes.ValidationAttributes;
+using System.Linq;
 using GovUkDesignSystem.ModelBinders;
 using Microsoft.AspNetCore.Mvc;
 using WhlgPortalWebsite.BusinessLogic.Models;
@@ -9,12 +9,12 @@ using WhlgPortalWebsite.Enums;
 
 namespace WhlgPortalWebsite.Models;
 
-public class ConfirmCodesToDeliveryPartnerViewModel: IValidatableObject
+public class ConfirmCodesToDeliveryPartnerViewModel : IValidatableObject
 {
     public User User { get; set; }
     public string Code { get; set; }
     public AuthorityType AuthorityType { get; set; }
-    
+
     [ModelBinder(typeof(GovUkCheckboxBoolBinder))]
     public bool IsConfirmed { get; set; }
 
@@ -27,13 +27,36 @@ public class ConfirmCodesToDeliveryPartnerViewModel: IValidatableObject
             _ => throw new InvalidOperationException("Unknown authority type")
         };
     }
-    
+
+    public string GetAuthorityTypePlural()
+    {
+        return AuthorityType switch
+        {
+            AuthorityType.LocalAuthority => "Local Authorities",
+            AuthorityType.Consortium => "Consortia",
+            _ => throw new InvalidOperationException("Unknown authority type")
+        };
+    }
+
+    public IEnumerable<string> GetAllManagedLocalAuthorityNames()
+    {
+        return AuthorityType switch
+        {
+            AuthorityType.LocalAuthority => [GetAuthorityName()],
+            AuthorityType.Consortium =>
+                ConsortiumData
+                    .ConsortiumCustodianCodesIdsByConsortiumCode[Code]
+                    .Select(custodianCode => LocalAuthorityData.LocalAuthorityNamesByCustodianCode[custodianCode]),
+            _ => throw new InvalidOperationException("Unknown authority type")
+        };
+    }
+
     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
     {
         if (!IsConfirmed)
         {
             yield return new ValidationResult(
-                "You must confirm the user has signed the DSA contract to onboard.",
+                "You must confirm the assignment to onboard.",
                 new[] { nameof(IsConfirmed) });
         }
     }
